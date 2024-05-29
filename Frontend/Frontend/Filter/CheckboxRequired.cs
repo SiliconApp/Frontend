fdfd244757b39d5c4c523f3535ff919f9bd0d798
kushtrim <kushtrim_shabooni99@hotmail.com>
2024-05-29 12:48:21 +0200

    Add project files.

---
+++ Frontend/Frontend/Filter/CheckboxRequired.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Frontend.Filter;
+
+public class CheckboxRequired : ValidationAttribute
+{
+	public override bool IsValid(object? value) => value is bool b && b;
+		
+}
